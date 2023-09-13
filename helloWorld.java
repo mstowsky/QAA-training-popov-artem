51d99942525cabdeb0b23afe599a01514d1c652b
Popov Artem <dflyerggl@gmail.com>
2023-09-13 21:34:30 +0400

    Добавил вывод рандомного числа

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("----------");
         System.out.println("Hello, World!");
         System.out.println("This is another string.");
         System.out.println("----------");
+        System.out.println("Random number: " + Math.random());
     }
 }
