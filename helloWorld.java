0e4a1bc7cce54d93dd3fc0c0f1f63c49254a213e
Popov Artem <dflyerggl@gmail.com>
2023-09-13 22:47:11 +0400

    Оставил только одну команду - вывод random.

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("----------");
-        System.out.println("Hello, World!");
-        System.out.println("This is another string.");
-        System.out.println("----------");
         System.out.println("Random number: " + Math.random());
     }
 }
