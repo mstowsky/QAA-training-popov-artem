96fde9009705ec32ec664c461bfbfe76425e0133
Popov Artem <dflyerggl@gmail.com>
2023-09-13 22:43:38 +0400

    Почистил код, убрал лишнее.

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,6 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("----------");
         System.out.println("Hello, World!");
-        System.out.println("This is another string.");
-        System.out.println("----------");
-        System.out.println("Random number: " + Math.random());
+        System.out.println("Random number is " + Math.random());
     }
 }
