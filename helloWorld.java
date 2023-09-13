5856510639b7c46782619ea04c1dd4ec24d6bed2
Popov Artem <dflyerggl@gmail.com>
2023-09-13 21:41:28 +0400

    Добавил вывод еще одной строки после hello wolrd.

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("----------");
         System.out.println("Hello, World!");
+        System.out.println("This is another string.");
         System.out.println("----------");
     }
 }
